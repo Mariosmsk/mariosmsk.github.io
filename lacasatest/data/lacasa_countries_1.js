9a327be8b36b0655b29a1fe13eaa64a1b4d65814
mariosmsk <mariosmsk@gmail.com>
2019-08-03 13:34:22 +0300

    lacasa_test_commit

---
+++ lacasatest/data/lacasa_countries_1.js
@@ -0,0 +1,21 @@
+var json_lacasa_countries_1 = {
+"type": "FeatureCollection",
+"name": "lacasa_countries_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Country": "Lisbon", "Photo": "<img src =\".\/images\/lisboa.png\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ -9.139319461680358, 38.7223259487983 ] } },
+{ "type": "Feature", "properties": { "Country": "Moscow", "Photo": "<img src =\".\/images\/moscow.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 37.6173, 55.74870180614937 ] } },
+{ "type": "Feature", "properties": { "Country": "Berlin", "Photo": "<img src =\".\/images\/berlin.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 13.406030964974672, 52.520448660683421 ] } },
+{ "type": "Feature", "properties": { "Country": "Nairobi", "Photo": "<img src =\".\/images\/nairobi.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 36.8219, -1.295765653243283 ] } },
+{ "type": "Feature", "properties": { "Country": "Rio", "Photo": "<img src =\".\/images\/rio.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ -43.172953635529851, -22.906704461712454 ] } },
+{ "type": "Feature", "properties": { "Country": "Denver", "Photo": "<img src =\".\/images\/denver.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ -104.990299435931789, 39.739198426331413 ] } },
+{ "type": "Feature", "properties": { "Country": "Stockholm", "Photo": "<img src =\".\/images\/stockholm.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 18.0686, 59.329300813856889 ] } },
+{ "type": "Feature", "properties": { "Country": "Helsinki", "Photo": "<img src =\".\/images\/helsinki.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 24.938400004252959, 60.169900013928441 ] } },
+{ "type": "Feature", "properties": { "Country": "Palermo", "Photo": "<img src =\".\/images\/palermo.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 13.3615, 38.115700003780411 ] } },
+{ "type": "Feature", "properties": { "Country": "Oslo", "Photo": "<img src =\".\/images\/oslo.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 10.752200001418819, 59.913900000728077 ] } },
+{ "type": "Feature", "properties": { "Country": "Tokyo", "Photo": "<img src =\".\/images\/tokyo.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 139.769000000177016, 35.680399999590684 ] } },
+{ "type": "Feature", "properties": { "Country": "Bogota", "Photo": "<img src =\".\/images\/bogota.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ -74.072099999996496, 4.711000000001377 ] } },
+{ "type": "Feature", "properties": { "Country": "Marseille", "Photo": "<img src =\".\/images\/marseille.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 5.36980000000012, 43.2965 ] } },
+{ "type": "Feature", "properties": { "Country": "Cincinnati", "Photo": "<img src =\".\/images\/cincinati.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ -84.512, 39.103100000000232 ] } }
+]
+}
---
+++ lacasatest/data/profesor_2.js
@@ -0,0 +1,8 @@
+var json_profesor_2 = {
+"type": "FeatureCollection",
+"name": "profesor_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "EL.Profesor", "Photo": "<img src =\".\/images\/elprofesor.jpg\"  width=\"300\" height=\"225\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 0.150578885940189, 81.862119676842823 ] } }
+]
+}
